ae031c52f6c498e4915515c90da886bbba96a29f
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2025-06-20 14:21:46 +0200

    set up axum server with a simple GET "/" route

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use axum::{
+    Router,
+    routing::{delete, get, post, put},
+}; 
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new()
+        .route("/", get("Hello, world!"));
+
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
